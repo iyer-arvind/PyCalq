27ee839f5d51db74ec600676f5c717465516acc1
arvind iyer <iyer.arvind.sundaram@gmail.com>
2016-05-09 06:34:25 +0100

    Code cleanups and a loop for absolute errors.

--- PyCalq.py
+++ PyCalq.py
@@ -13,9 +13,10 @@ ureg = UnitRegistry()
 lcls = {'pi': np.pi, 'sqrt': np.sqrt, 'pow': np.power,
         '_U': ureg}
 
+np.seterr(all='ignore')
+
 
 def parse_input(file_name):
-    u_re = re.compile(r'\[([^]]*)\]')
     with open(sys.argv[1], 'r') as fh:
         eqns = []
         STATE = 'E'
@@ -57,10 +58,10 @@ def solve(eqns, vardict, pardict):
     def errsq(testvars, *args):
         lcl = dict(lcls)
         for n, m, u in zip(var_n, testvars, var_u):
-            lcl[n] = m if u.dimensionless else u * m 
+            lcl[n] = m if u.dimensionless else u * m
 
         lcl.update(zip(pardict.keys(), args))
-    
+
         sqerr = 0
         for l, r in eqns:
             lv = eval(l, lcl)
@@ -69,34 +70,41 @@ def solve(eqns, vardict, pardict):
                 lv = ureg.Quantity(lv)
                 rv = ureg.Quantity(rv)
                 assert (lv.u/rv.u).dimensionless,\
-                        '"{} = {}" units dont match ({}, {})'.format(
-                                l, r, lv.to_base_units().u,
-                                rv.to_base_units().u)
+                    '"{} = {}" units dont match ({}, {})'.format(
+                        l, r, lv.to_base_units().u,
+                        rv.to_base_units().u)
 
             mv = max(abs(lv), abs(rv))
             sqerr += float(((lv - rv) / mv) ** 2)
 
         return sqerr
-    
-    var_u = tuple(v.u if type(v) is ureg.Quantity else ureg.Quantity(1) 
+
+    var_u = tuple(v.u if type(v) is ureg.Quantity else ureg.Quantity(1)
                   for v in vardict.values())
 
-    var_m = tuple(v.m if type(v) is ureg.Quantity else v 
+    var_m = tuple(v.m if type(v) is ureg.Quantity else v
                   for v in vardict.values())
 
     var_n = tuple(vardict.keys())
 
-    vals = fmin(errsq, var_m,
-                tuple(pardict.values()),
-                ftol=1e-12, maxfun=1000000, maxiter=1000000)
+    while True:
+        var_m = fmin(errsq, var_m,
+                     tuple(pardict.values()), disp=False,
+                     ftol=1e-12, xtol=1e-12, maxfun=1000000, maxiter=1000000)
+
+        err = errsq(var_m, *tuple(pardict.values()))
+
+        if err < 1e-8:
+            break
 
     return dict((n, m if u.dimensionless else m*u)
-                for n, m, u in zip(var_n, vals, var_u))
-        
+                for n, m, u in zip(var_n, var_m, var_u))
+
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
-    parser = ArgumentParser('Simultaneous NonLinear Equation Solver with Units')
+    parser = ArgumentParser(
+        'Simultaneous NonLinear Equation Solver with Units')
     parser.add_argument('input', type=str)
     args = parser.parse_args()
 
